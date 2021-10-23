733371c41f4576823a554edf61c23576c33500ba
JasserRomero247 <jasseromero08@gmail.com>
2021-10-23 17:45:45 -0600

    Agregar archivos de proyecto.

---
+++ Domain/Entities/Donante.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Donante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cedula { get; set; }
+        public Genero Genero { get; set; }
+        public string Ocupacion { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public FactorRH FactorRH { get; set; }
+        public GrupoSanguineo GrupoSanguineo { get; set; }
+        public EstadoCivil EstadoCivil { get; set; }
+        public string Correo { get; set; }
+
+    }
+}
---
+++ Domain/Enums/GrupoSanguineo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Enums
+{
+    public enum GrupoSanguineo
+    {
+        A_Positivo,
+        A_Negativo,
+        AB_Positivo,
+        AB_Negativo,
+        O_Positivo,
+        O_Negativo
+    }
+}
