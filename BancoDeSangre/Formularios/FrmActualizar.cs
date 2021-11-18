7a60152aa811c36c9ee5548eabb18d70d36b668a
JasserRomero247 <jasseromero08@gmail.com>
2021-11-18 00:22:22 -0600

    Pequeño cambio

--- BancoDeSangre/Formularios/FrmActualizar.cs
+++ BancoDeSangre/Formularios/FrmActualizar.cs
@@ -8,6 +8,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -24,6 +25,46 @@ namespace BancoDeSangre.Formularios
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
+            if (cmbEstadoCivil.SelectedIndex == -1)
+            {
+                MessageBox.Show("Error, todos los campos son necesarios", "Mensaje de Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (cmbFactorRH.SelectedIndex == -1)
+            {
+                MessageBox.Show("Error, todos los campos son necesarios", "Mensaje de Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (cmbGenero.SelectedIndex == -1)
+            {
+                MessageBox.Show("Error, todos los campos son necesarios", "Mensaje de Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (cmbGrupoSanguineo.SelectedIndex == -1)
+            {
+                MessageBox.Show("Error, todos los campos son necesarios", "Mensaje de Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string patronCedula = @"\A[0-9]{3}(\-)[0-9]{6}(\-)[0-9]{4}[A-Z]\Z";
+            if (!Regex.IsMatch(txtCedula.Text, patronCedula))
+            {
+                MessageBox.Show("Error, cédula ingresada invalida", "Mensaje de Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string patronTelefono = @"\A[0-9]{7,10}\z";
+            if (!Regex.IsMatch(txtTelefono.Text, patronTelefono))
+            {
+                MessageBox.Show("Error, telefono ingresado invalido", "Mensaje de Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtApellidos.Text) || string.IsNullOrEmpty(txtCedula.Text)
+                || string.IsNullOrEmpty(txtCorreo.Text) || string.IsNullOrEmpty(txtOcupacion.Text) || string.IsNullOrEmpty(txtTelefono.Text))
+            {
+                MessageBox.Show("Error, se requieren de todos los campos", "Mensaje de Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
             Donante donante = new Donante()
             {
                 Id = Convert.ToInt32(txtId.Text),
